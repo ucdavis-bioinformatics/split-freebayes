6b8bd8bd8062d3439e837b6587cadb7d4b927653
Nikhil Joshi <najoshi@ucdavis.edu>
2019-08-14 15:18:42 -0700

    adding outdir

--- fb_join.pl
+++ fb_join.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
-$OUTDIR="variant";
+$INDIR="variant";
+$OUTDIR="combined";
 
 if (scalar(@ARGV) < 2) {
     print STDERR "Usage: $0 <chr/pos file> <window size>\n";
@@ -17,13 +18,13 @@ while (<$chrfile>) {
 
     for ($i=$beg; $i<=$end; $i+=$window) {
         if ($i+$window <= $end) {
-            $fn = "$OUTDIR/fb.${chr}_$i-".($i+$window-1).".vcf";
+            $fn = "$INDIR/fb.${chr}_$i-".($i+$window-1).".vcf";
             if (! -e $fn || -z $fn) {
                 print STDERR "ERROR: $fn does not exist or is zero size.\n";
                 $flag=1;
             }
         } else {
-            $fn = "$OUTDIR/fb.${chr}_$i-$end.vcf";
+            $fn = "$INDIR/fb.${chr}_$i-$end.vcf";
             if (! -e $fn || -z $fn) {
                 print STDERR "ERROR: $fn does not exist or is zero size.\n";
                 $flag=1;
@@ -49,19 +50,19 @@ while (<$chrfile>) {
     for ($i=$beg; $i<=$end; $i+=$window) {
         if ($i+$window <= $end) {
             if ($flag==0) {
-                print "grep ^# variant/fb.${chr}_$i-".($i+$window-1).".vcf > combined/$chr.ALL.vcf\ncat ";
+                print "grep ^# $INDIR/fb.${chr}_$i-".($i+$window-1).".vcf > $OUTDIR/$chr.ALL.vcf\ncat ";
                 $flag = 1;
             }
-            print "variant/fb.${chr}_$i-".($i+$window-1).".vcf ";
+            print "$INDIR/fb.${chr}_$i-".($i+$window-1).".vcf ";
         } else {
             if ($flag==0) {
-                print "grep ^# variant/fb.${chr}_$i-$end.vcf > combined/$chr.ALL.vcf\ncat ";
+                print "grep ^# $INDIR/fb.${chr}_$i-$end.vcf > $OUTDIR/$chr.ALL.vcf\ncat ";
                 $flag = 1;
             }
-            print "variant/fb.${chr}_$i-$end.vcf ";
+            print "$INDIR/fb.${chr}_$i-$end.vcf ";
         }
     }
 
-    print "| grep -v ^# >> combined/$chr.ALL.vcf\n";
+    print "| grep -v ^# >> $OUTDIR/$chr.ALL.vcf\n";
 }
 close($chrfile);
